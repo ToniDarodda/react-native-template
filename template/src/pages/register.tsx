89b85ddaeeeeff9c7638f7e4c52bfa1e46fd9648
Toni Da Rodda <toni.da.rodda.pro@gmail.com>
2024-08-06 13:54:09 +0200

    feat: add missing utils and configurations

--- template/src/pages/register.tsx
+++ template/src/pages/register.tsx
@@ -1,13 +1,34 @@
 import React from 'react';
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 import { useTranslation } from 'react-i18next';
-import { SafeAreaView, Text } from 'react-native';
+import { SafeAreaView, View } from 'react-native';
 
-export const RegisterScreen = () => {
-    const { t } = useTranslation();
+import { MainRootStackParamList } from '../navigations/main-root-stack';
+import { Button } from '../components/button';
+
+type Props = {
+    navigation: NativeStackNavigationProp<MainRootStackParamList, 'Register'>;
+};
+
+export const RegisterScreen: React.FC<Props> = ({ navigation }) => {
+    const { t } = useTranslation('register');
+
+    const navigateLogin = () => {
+        navigation.navigate('Login');
+    };
 
     return (
-        <SafeAreaView>
-            <Text>{t('OS')}</Text>
+        <SafeAreaView
+            style={{ height: 600, justifyContent: 'center', marginTop: 100 }}>
+            <View
+                style={{
+                    width: '100%',
+                    height: '100%',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                }}>
+                <Button text={t('register_button')} onPress={navigateLogin} />
+            </View>
         </SafeAreaView>
     );
 };
