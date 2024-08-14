cb6b10d3bcd6943a438a460eb5d515189162809a
Toni Da Rodda <toni.da.rodda.pro@gmail.com>
2024-08-14 12:18:22 +0200

    feat: add new searchbar component and traduction

---
+++ template/src/components/search-bar.tsx
@@ -0,0 +1,62 @@
+import React, { useRef } from 'react';
+import { StyleSheet, TextInput, TouchableOpacity } from 'react-native';
+import Icon from 'react-native-vector-icons/AntDesign';
+import { useTranslation } from 'react-i18next';
+
+import { HStack } from './hstack';
+import { Text } from '../styles/text';
+import { ICommonComponents } from '../types/common-component';
+
+interface ISearchBarProps extends ICommonComponents {
+    onSearch?: (val: string) => void;
+}
+
+const SearchBarComponent: React.FC<ISearchBarProps> = ({ onSearch }) => {
+    const { t } = useTranslation('component');
+
+    const inputRef = useRef<TextInput>(null);
+
+    const handlePress = () => {
+        inputRef.current?.focus();
+    };
+
+    return (
+        <TouchableOpacity onPress={handlePress} style={{ width: '100%' }}>
+            <HStack style={searchBarStyle.container}>
+                <TextInput
+                    ref={inputRef}
+                    placeholder={t('search_bar_placeholder')}
+                    placeholderTextColor={'#858489'}
+                    style={Text.medium}
+                    onChangeText={onSearch}
+                />
+                <Icon
+                    name={'search1'}
+                    size={20}
+                    color="#c8cdd6"
+                    style={searchBarStyle.icon}
+                />
+            </HStack>
+        </TouchableOpacity >
+    );
+};
+
+export const SearchBar = React.memo(SearchBarComponent);
+
+const searchBarStyle = StyleSheet.create({
+    container: {
+        width: '100%',
+        height: 38,
+        borderRadius: 4,
+        padding: 12,
+        paddingLeft: 44,
+        justifyContent: 'flex-start',
+        backgroundColor: '#f2f2f2'
+    },
+    icon: {
+        position: 'absolute',
+        left: 12,
+        zIndex: 1,
+        color: '#858489'
+    },
+});
